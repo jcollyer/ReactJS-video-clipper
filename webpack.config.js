f3a9f17366d2b3460da57c881d72f230974889db
jcollyer <collyerdesign@gmail.com>
2016-02-13 09:19:12 -0800

    upgrade to react v0.14

--- webpack.config.js
+++ webpack.config.js
@@ -14,30 +14,7 @@ module.exports = {
         loaders: [
         'react-hot', 'jsx-loader'
         ]
-      }, {
-        test: /\.less$/, loader: 'style-loader!css-loader!less-loader'
-      }, {
-        test: /\.svg/,
-        loader: 'url-loader?limit=10000&minetype=image/svg+xml'
-      }, {
-        test: /\.eot/,
-        loader: 'url-loader?limit=10000&minetype=application/vnd.ms-fontobject'
-      }, {
-        test: /\.ttf|otf/,
-        loader: 'url-loader?limit=10000&minetype=application/font-sfnt'
-      }, {
-        test: /\.woff/,
-        loader: 'url-loader?limit=10000&minetype=application/font-woff'
-      }, {
-        test: /\.gif/,
-        loader: 'url-loader?limit=10000&minetype=image/gif'
-      }, {
-        test: /\.jpg/,
-        loader: 'url-loader?limit=10000&minetype=image/jpg'
-      }, {
-        test: /\.png/,
-        loader: 'url-loader?limit=10000&minetype=image/png'
-    }]
-  },
-  plugins: [new Webpack.HotModuleReplacementPlugin()]
+      }
+    ]
+  }
 };
